45935a9ee334efe37aa967e3e6e45319b3f7cb2d
Gabriela Melo <gmelo@scopus.com.br>
2019-01-18 21:45:44 +0000

    added git config

---
+++ git_config.sh
@@ -0,0 +1,14 @@
+git config --global user.email "gabimelo10@gmail.com"
+git config --global user.name "Gabriela Melo"
+
+# Assumes SSH key does not exist yet
+ssh-keygen -t rsa -b 4096 -C "gabimelo10@gmail.com"
+
+if ! eval $(ssh-agent -s) | grep -q 'Agent pid'; then
+   echo "SSH Agent seems not to be running, please verify";
+   exit
+fi
+
+ssh-add ~/.ssh/id_rsa
+
+cat ~/.ssh/id_rsa.pub
