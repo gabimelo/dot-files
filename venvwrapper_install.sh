43496ed85912665f1d285b852fc6587f073c05b9
Gabriela Melo <>
2019-08-20 18:02:31 -0300

    add instructions for venv install

---
+++ venvwrapper_install.sh
@@ -0,0 +1,6 @@
+sudo apt install python-pip
+sudo pip install virtualenv virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+# put line above in bash profile
+# to create new env: mkvirtualenv --py python3.7 <env_name>
+
