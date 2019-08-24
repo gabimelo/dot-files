eda5245af71acd058c6e4f6b61c986b631a241a7
Gabriela Melo <gabimelo@users.noreply.github.com>
2019-08-24 15:44:57 -0300

    Update custom_aliases.zsh

--- custom_aliases.zsh
+++ custom_aliases.zsh
@@ -5,11 +5,10 @@ alias jl="jupyter lab"
 alias ca="conda activate"
 alias jup_lab_ext="conda install -c conda-forge nodejs && jupyter labextension install @jupyterlab/toc && jupyter labextension install @krassowski/jupyterlab_go_to_definition"
 
+alias zsh_update='source ~/.zshrc'
+alias zsh_edit='subl ~/.zshrc'
+
 alias copy='xclip -sel clip'
-
-alias update_zsh='source ~/.zshrc'
-alias edit_zsh='subl ~/.zshrc'
-
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 
