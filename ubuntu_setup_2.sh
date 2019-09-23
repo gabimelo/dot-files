54aff348f9800f29b30fbf707e5b21268cde7049
Gabriela Melo <gabimelo@users.noreply.github.com>
2019-09-23 15:25:41 -0300

    Update ubuntu_setup_2.sh

--- ubuntu_setup_2.sh
+++ ubuntu_setup_2.sh
@@ -1,5 +1,4 @@
-sudo apt-get install vim
-sudo apt-get install tmux
+sudo apt-get install vim tmux jq tree xclip
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -13,10 +12,9 @@ sudo apt-get update
 sudo apt-get install sublime-text
 
 # Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 # Anaconda --> manually download and then bash Anaconda-latest-Linux-x86_64.sh
 
