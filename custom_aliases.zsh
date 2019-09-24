18766e35e7e450c75ef9d774f18ce6979c9bd0f8
Gabriela Melo <gabimelo10@gmail.com>
2019-09-24 20:50:27 -0300

    update zsh_edit and add aliases_edit

--- custom_aliases.zsh
+++ custom_aliases.zsh
@@ -6,7 +6,8 @@ alias ca="conda activate"
 alias jup_lab_ext="conda install -c conda-forge nodejs && jupyter labextension install @jupyterlab/toc && jupyter labextension install @krassowski/jupyterlab_go_to_definition"
 
 alias zsh_update='source ~/.zshrc'
-alias zsh_edit='subl ~/.zshrc'
+alias zsh_edit='vim ~/.zshrc'
+alias aliases_edit='vim $ZSH_CUSTOM/custom_aliases.zsh'
 
 alias copy='xclip -sel clip'
 alias pbcopy='xclip -selection clipboard'
