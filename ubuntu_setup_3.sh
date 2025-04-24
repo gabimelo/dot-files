485b39cccb54f3df32d6388a352c815382d08e5f
Gabriela Melo <gabimelo@users.noreply.github.com>
2025-04-24 18:34:51 -0300

    machine setup updates

--- ubuntu_setup_3.sh
+++ ubuntu_setup_3.sh
@@ -1,47 +1,47 @@
 # TODO add conditionals based on OS
-
 # TODO add symlinking of config files
 
-sudo apt-get install vim tmux jq tree xclip htop neovim fzf
+## mac
 
-mkdir ~/Projects
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo >> /Users/gabimelo/.zprofile
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/gabimelo/.zprofil
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install tmux tree htop neovim fzf git-spice git-delta gh pyenv fd ripgrep fnm
+
+## ubuntu
+# sudo apt-get install vim tmux jq tree xclip htop neovim fzf
+# curl https://pyenv.run | bash
+
+# generics
+
 mkdir ~/src
-
- # vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# nvim config
-mkdir -p ~/.config/nvim
-cp conf/init.vim ~/.config/nvim/init.vim
-
-# tmux conf
+cp -r nvim ~/.config/
 cp conf/.tmux.conf ~/
+echo '
+[core]
+    pager = delta
 
-# TODO git spice
+[interactive]
+    diffFilter = delta --color-only
 
-# TODO switch to delta
-# diff-so-fancy
-git clone https://github.com/so-fancy/diff-so-fancy ~/.diff-so-fancy
-chmod +x ~/.diff-so-fancy
-# do I need to export PATH=$PATH:~/.diff-so-fancy ?
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
+[delta]
+    navigate = true  # use n and N to move between diff sections
+    dark = true      # or light = true, or omit for auto-detection
+
+[merge]
+    conflictstyle = zdiff3
+
+[diff]
+    colorMoved = default' >> .gitconfig
 
 # zsh-completions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # pyenv
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-source ~/.zshrc
-pyenv install 3.13
-pyenv virtualenv 3.13 neovimpy13
-pyenv activate neovimpy13
-pip install neovim typing-extensions jedi
+# use it like:
+# pyenv install 3.13
+# pyenv virtualenv 3.13 neovimpy13
+# pyenv activate neovimpy13
